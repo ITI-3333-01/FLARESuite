9a8e346291d83c99bd8896c851390c172179f07e
Austin Mayes <austinlm@Me.com>
2019-01-18 13:00:54 -0600

    SQL Support

--- aggregator/src/main/java/edu/trevecca/flare/aggregator/PacketRedisHandler.java
+++ aggregator/src/main/java/edu/trevecca/flare/aggregator/PacketRedisHandler.java
@@ -4,12 +4,19 @@ import com.google.gson.JsonObject;
 import edu.trevecca.flare.core.redis.RedisHandler;
 import java.io.File;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.time.Instant;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Listens for {@link edu.trevecca.flare.core.transfer.PackerDumpRedisMessage} and handles them.
  */
 public class PacketRedisHandler implements RedisHandler {
+
+    private final String DUMP_INSERT = "INSERT into dumps (time, error, total) VALUES (?, ?, ?)";
 
     private final File out;
     private final AtomicInteger received = new AtomicInteger();
@@ -24,10 +31,23 @@ public class PacketRedisHandler implements RedisHandler {
 
     @Override public void handle(JsonObject json) {
         try {
+            try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost/flare?" +
+                    "user=root&password=BLAZE")) {
+                PreparedStatement insert = con.prepareStatement(DUMP_INSERT);
+
+                int size = json.get("outbound").getAsJsonArray().size() + json.get("inbound").getAsJsonArray().size();
+                insert.setDate(1, new Date(Instant.now().getEpochSecond()));
+                insert.setInt(2, json.get("bad-nets").getAsInt());
+                insert.setInt(3, size);
+
+                insert.execute();
+            }
+            /*
             try (PrintWriter writer = new PrintWriter(out)) {
                 json.addProperty("files-recorded", this.received.incrementAndGet());
                 writer.println(json);
             }
+            */
         }
         catch (Exception e) {
             Main.logger.severe("Failed to handle packet data!");
